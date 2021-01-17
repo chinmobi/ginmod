d952819ced171f5225e225f058e4228b32db697e
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-17 07:56:29 -0500

    Define error types base

---
+++ errors/base.go
@@ -0,0 +1,29 @@
+// Copyright 2020 Zhaoping Yu.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package errors
+
+// Common original error types base.
+type OriginalErrorBase struct {
+	Name    string  `json:"name"`
+	Message string  `json:"message"`
+}
+
+func (err *OriginalErrorBase) Error() string {
+	return err.Name + ": \"" + err.Message + "\""
+}
+
+// Common caused error types base.
+type CausedErrorBase struct {
+	OriginalErrorBase
+	Cause   error   `json:"-"`
+}
+
+func (err *CausedErrorBase) Error() string {
+	str := err.OriginalErrorBase.Error()
+	if err.Cause != nil {
+		str += " Caused by: {" + err.Cause.Error() + "}"
+	}
+	return str
+}
