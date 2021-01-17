2973164136b9d272e1822d456fa1107748d67826
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-17 11:07:04 -0500

    Define the Entry for adapters

---
+++ middleware/entry.go
@@ -0,0 +1,49 @@
+// Copyright 2020 Zhaoping Yu.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package middleware
+
+// Entry for adapters grouped by category.
+type Entry struct {
+	category  string
+	adapters  []Adapter
+	next      *Entry
+}
+
+// Create entry
+func newEntry(category string) *Entry {
+	e := &Entry{
+		category: category,
+	}
+	return e
+}
+
+// Create next entry
+func (e *Entry) newNext(category string) *Entry {
+	n := &Entry{
+		category: category,
+	}
+	e.next = n
+	return n
+}
+
+// Get the entry's category
+func (e *Entry) Category() string {
+	return e.category
+}
+
+// Get the next entry
+func (e *Entry) Next() *Entry {
+	return e.next
+}
+
+// Add adapter to the entry
+func (e *Entry) addAdapter(adapter ...Adapter) {
+	e.adapters = append(e.adapters, adapter...)
+}
+
+// Get the entry's adapters
+func (e *Entry) Adapters() []Adapter {
+	return e.adapters
+}
